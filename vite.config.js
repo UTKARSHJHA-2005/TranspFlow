529cdc2ce689206f02ada6e740825132e4d98230
Utkarsh Jha <jha.utkarsh2005@gmail.com>
2025-04-07 13:39:53 +0530

    UPdated

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/supply-chain",
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  esbuild: {
+    loader: 'jsx', // Ensures JSX is processed correctly
+  },
 })
