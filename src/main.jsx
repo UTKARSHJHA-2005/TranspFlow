be7c747b24f526ab3225282760eae1c2a54771c3
UTKARSH JHA <147072811+UTKARSHJHA-2005@users.noreply.github.com>
2025-04-07 14:54:51 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename='/TranspFlow'>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>,
+  </StrictMode>
 )
