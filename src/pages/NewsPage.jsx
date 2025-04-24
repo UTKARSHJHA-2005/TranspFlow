20e78cb37be695a06a23ed4b87c59092547cc870
UTKARSH JHA <147072811+UTKARSHJHA-2005@users.noreply.github.com>
2025-04-24 20:13:07 +0530

    Update NewsPage.jsx

--- src/pages/NewsPage.jsx
+++ src/pages/NewsPage.jsx
@@ -185,12 +185,14 @@ export default function LogisticsNews() {
                 </li>
           </Link>
           <li className="py-2">
-            <button className="w-full bg-gradient-to-r from-blue-600 to-blue-800 text-white px-4 py-3 rounded-lg shadow-md hover:shadow-lg transition duration-300 flex items-center justify-center">
-              <span>Contact Us</span>
-              <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M14 16L18 12M18 12L14 8M18 12H6" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-              </svg>
-            </button>
+            <Link to="/contact">
+             <button className="w-full bg-gradient-to-r from-blue-600 to-blue-800 text-white px-4 py-3 rounded-lg shadow-md hover:shadow-lg transition duration-300 flex items-center justify-center">
+               <span>Contact Us</span>
+               <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                 <path d="M14 16L18 12M18 12L14 8M18 12H6" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+               </svg>
+             </button>
+            </Link>
           </li>
         </ul>
       </div>
